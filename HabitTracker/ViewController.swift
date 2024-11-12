4d1680e2c74447945cc301189da4db0ba6740971
jayden Sawyer <jdnswr@gmail.com>
2024-11-11 20:29:14 -0800

    yo

--- HabitTracker/ViewController.swift
+++ HabitTracker/ViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 
-
-class Habit : Codable{
+class Habit: Codable {
     var name: String
     var description: String
     
@@ -16,42 +15,36 @@ class Habit : Codable{
         self.name = name
         self.description = description
     }
-    
 }
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-
     var habits: [Habit] = []
-    
     let defaults = UserDefaults.standard
-
     @IBOutlet weak var tableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let hab = defaults.array(forKey: "myHabits"){
-            habits = hab as! [Habit]
+
+        if let savedData = defaults.data(forKey: "myHabits") {
+            let decoder = JSONDecoder()
+            if let loadedHabits = try? decoder.decode([Habit].self, from: savedData) {
+                habits = loadedHabits
+            }
         }
+
+        if habits.isEmpty {
+            let sleep = Habit(name: "Sleep", description: "Sleep for 8 hours today")
+            let exercise = Habit(name: "Exercise", description: "Go for a 30-minute run")
+            let meditation = Habit(name: "Meditation", description: "Do 10 minutes of meditation")
+            habits.append(sleep)
+            habits.append(exercise)
+            habits.append(meditation)
+        }
+
         tableView.delegate = self
         tableView.dataSource = self
-        
-       
-        
-        let sleep = Habit(name: "sleep", description: "sleep for 8 hours today")
-        
-        let exercise = Habit(name: "Exercise", description: "Go for a 30 minute run")
-
-        
-        let meditation = Habit(name: "Meditation", description: "Do 10 minutes of meditation")
-        habits.append(meditation)
-        habits.append(exercise)
-        habits.append(sleep)
-        
-      
     }
-
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return habits.count
@@ -66,12 +59,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @IBAction func saveButton(_ sender: UIButton) {
-        defaults.set(habits, forKey: "myHabits")
-    }
-    @IBAction func resetButton(_ sender: UIButton) {
-        defaults.removeObject(forKey: "myHabits")
+        let encoder = JSONEncoder()
+        if let encodedData = try? encoder.encode(habits) {
+            defaults.set(encodedData, forKey: "myHabits")
+        }
     }
     
+    @IBAction func resetButton(_ sender: UIButton) {
+        defaults.removeObject(forKey: "myHabits")
+        habits.removeAll()
+        tableView.reloadData()
+    }
     
     func addHabit(name: String, description: String) {
         let newHabit = Habit(name: name, description: description)
